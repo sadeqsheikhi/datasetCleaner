6fd937f03e7606098cd0a9154fa63b9343661d8b
sadeqsheikhi <sadeghshaikhi77@gmail.com>
2022-09-13 09:57:45 +0430

    Added Keyboard Shortcuts

--- app.js
+++ app.js
@@ -15,4 +15,13 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#modal-img').addEventListener('click', e => {
         e.stopPropagation()
     })
+
+    document.addEventListener('keydown', event => {
+        if (event.ctrlKey && event.code === 'ArrowRight') {
+            document.querySelector('#next-btn').click()
+        } else if (event.ctrlKey && event.code === 'ArrowLeft') {
+            document.querySelector('#prev-btn').click()
+        }
+    })
+
 })
